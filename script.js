8098c6001c7940577b17aa7101d6f512414dfc87
Adarsh Unni <adarshunni.work@gmail.com>
2023-09-29 16:40:56 +0530

    Create function to store user data into an array

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 const myLibrary = [];
 
-function book(titile,author,pages,yesno){
+function book(titile,author,pages){
     this.titile = titile;
     this.author = author;
     this.pages = pages;
-    this.yesno = yesno;
 }
 
 function addBookToLibrary(book){    
     myLibrary.push(book)
+    display(myLibrary)
 }
 
 function display(myLibrary){
     n = myLibrary.length
-    for (let i=0; i<n; i++){
-        console.log(myLibrary[i])
-    }
+    console.log(myLibrary[n-1])
 }
 
-const form = document.querySelector('.details')
-form.style.display = 'none'
-
+const form = document.querySelector('.form-details')
 const addBook = document.querySelector('.addBook-button')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const pages = document.getElementById('pages')
+const dialog = document.querySelector('dialog')
+
+
 addBook.addEventListener('click', () => {
-    form.style.display = 'block'
+    form.reset()
+    dialog.showModal()
+    })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    dialog.close()
+    let books = new book(title.value, author.value, pages.value)
+    addBookToLibrary(books)
 })
-
-
-
-
-
 
